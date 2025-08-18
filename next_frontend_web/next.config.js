2ca9320ce1a45db1146387d6f26a5d086ed8b4ea
FASALURAHMANMK <93432834+FASALURAHMANMK@users.noreply.github.com>
2025-08-18 22:37:50 +0400

    chore: update API config and add lint rules

--- next_frontend_web/next.config.js
+++ next_frontend_web/next.config.js
@@ -1,13 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
-    trailingSlash: true,
-    images: {
-      unoptimized: true
-    },
-    assetPrefix: process.env.NODE_ENV === 'production' ? './' : '',
-    basePath: '',
-    distDir: 'out'
-  }
-  
-  module.exports = nextConfig
+  output: 'export',
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
+  assetPrefix: process.env.NODE_ENV === 'production' ? './' : '',
+  basePath: '',
+  distDir: 'out',
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://localhost:8080/api/:path*',
+      },
+    ];
+  },
+};
+
+module.exports = nextConfig;
