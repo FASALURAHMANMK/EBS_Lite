f473aed18b7c212f5765843af3ed681c35a4805e
FASALURAHMANMK <93432834+FASALURAHMANMK@users.noreply.github.com>
2025-08-21 22:57:02 +0400

    Handle specific CORS origins

--- go_backend_rmt/internal/middleware/cors.go
+++ go_backend_rmt/internal/middleware/cors.go
@@ -12,8 +12,24 @@ import (
 // CORS middleware handles Cross-Origin Resource Sharing
 func CORS(cfg *config.Config) gin.HandlerFunc {
 	return func(c *gin.Context) {
-		// Add CORS headers
-		c.Header("Access-Control-Allow-Origin", strings.Join(cfg.AllowedOrigins, ", "))
+		origin := c.Request.Header.Get("Origin")
+		allowed := false
+		for _, o := range cfg.AllowedOrigins {
+			if o == origin {
+				allowed = true
+				break
+			}
+		}
+
+		if origin != "" && !allowed {
+			c.AbortWithStatus(http.StatusForbidden)
+			return
+		}
+
+		if allowed {
+			c.Header("Access-Control-Allow-Origin", origin)
+		}
+
 		c.Header("Access-Control-Allow-Methods", strings.Join(cfg.AllowedMethods, ", "))
 		c.Header("Access-Control-Allow-Headers", strings.Join(cfg.AllowedHeaders, ", "))
 		c.Header("Access-Control-Expose-Headers", "Content-Length, X-Total-Count")
