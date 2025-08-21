9a70656b72cd446a85e57c6fcbfe2489d2a8a3b0
FASALURAHMANMK <93432834+FASALURAHMANMK@users.noreply.github.com>
2025-08-21 22:17:03 +0400

    Consolidate migrations into schema

--- go_backend_rmt/migrations/002_add_created_updated_by_to_ledger_entries.sql
+++
@@ -1,6 +0,0 @@
-ALTER TABLE ledger_entries
-    ADD COLUMN created_by INTEGER REFERENCES users(user_id);
-ALTER TABLE ledger_entries
-    ADD COLUMN updated_by INTEGER REFERENCES users(user_id);
-UPDATE ledger_entries SET created_by = 1, updated_by = 1 WHERE created_by IS NULL;
-ALTER TABLE ledger_entries ALTER COLUMN created_by SET NOT NULL;
