a400f69d9b22e2c801da6750aaa74e1cd955380a
FASALURAHMANMK <93432834+FASALURAHMANMK@users.noreply.github.com>
2025-08-21 19:43:41 +0400

    Switch to server rendering for i18n

--- next_frontend_web/next.config.js
+++ next_frontend_web/next.config.js
@@ -3,13 +3,9 @@ const { i18n } = require('./next-i18next.config');
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   i18n,
-  output: 'export',
-  trailingSlash: true,
   images: {
     unoptimized: true,
   },
-  assetPrefix: process.env.NODE_ENV === 'production' ? './' : '',
-  basePath: '',
   distDir: 'out',
   async rewrites() {
     const apiProxyUrl = process.env.API_PROXY_URL || process.env.NEXT_PUBLIC_API_URL;
