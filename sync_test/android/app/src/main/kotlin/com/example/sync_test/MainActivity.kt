b28ea8ab28dacf69891c4172388011b87c376170
FASALURAHMANMK <fasalurahmanmkf@gmail.com>
2026-04-09 13:05:43 +0400

    .

--- sync_test/android/app/src/main/kotlin/com/example/sync_test/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.sync_test
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- sync_test/lib/utils/json.dart
+++
@@ -1,3 +0,0 @@
-import 'dart:convert';
-T decodeJson<T>(String s) => jsonDecode(s) as T;
-String encodeJson(Object v) => jsonEncode(v);
